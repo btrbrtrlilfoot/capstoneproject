a7d78f767d81463c6ecabe1da2d21da85eb9a677
Rachel Taverna <rtaverna96@gmail.com>
2020-02-04 15:52:48 -0500

    added one new line to user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -4,6 +4,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING
+  },
   email: {
     type: Sequelize.STRING,
     unique: true,
